47b3f3f6179f78fb6f01957c3ac327baeddd4174
yker14 <ykerkarod@gmail.com>
2020-12-05 00:41:59 -0500

    v20201205 v3 fixed sync route on backend

--- app/main.py
+++ app/main.py
@@ -184,12 +184,11 @@ def publish(publ_id):
         return ('A problem occurred while processing the request. \n Error Message: {}'.format(e.args), 400)
 
 
-@app.route('/publicaciones/<publ_id>/sync')
+@app.route('/publicaciones/<publ_id>/sync', methods=['POST'])
 def sync(publ_id):
 
     # Data modelo
-    data = {"EXITOOO, UPDATEADO"}
-    return (json.dumps(data),200)
+    return ('ID {} Inventario Actualizado'.format(publ_id),200)
     
 
 @app.route('/mlloggedid', methods=['GET'])
